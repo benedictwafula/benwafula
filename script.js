aa4be064f0bdc4b406dd7353c66703bdccaf0dc4
benedict wafula <wafulabenedict9@gmail.com>
2026-03-27 18:03:54 +0300

    Updated website

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function scrollToProjects() {
       behavior: "smooth"
   });
 }
+<script>
+  const menuToggle = document.getElementById('menuToggle');
+  const navMenu = document.getElementById('navMenu');
+
+  menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    menuToggle.setAttribute('aria-expanded', navMenu.classList.contains('show'));
+  });
+</script>
